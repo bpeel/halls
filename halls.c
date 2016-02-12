97ea12770bf7b2c25f48093632a386dfd5a0d496
Neil Roberts <neil@linux.intel.com>
2016-02-12 18:52:51 +0000

    Use image and shader from demo

--- halls.c
+++ halls.c
@@ -11,23 +11,114 @@ vert_shader_source[] =
         "#version 330\n"
         "\n"
         "layout(location = 0) in vec4 position;\n"
+        "#define tex_coords webgl_51798873aa8ff428\n"
+        "out vec2 tex_coords;\n"
         "\n"
         "void\n"
         "main()\n"
         "{\n"
         "        gl_Position = position;\n"
+        "        tex_coords = vec2(position.x, -position.y) / 2.0 + 0.5;\n"
         "}\n";
 
 static const char
 frag_shader_source[] =
-        "#version 330\n"
-        "\n"
-        "out vec4 color;\n"
-        "\n"
-        "void\n"
-        "main()\n"
+        "#version 130\n"
+        "out vec4 webgl_FragColor;\n"
+        "uniform mat4 webgl_2f0d57c31e3ad65f;\n"
+        "uniform vec3 webgl_fa78a8ce74d1aaee;\n"
+        "uniform float webgl_9f3d75d0c870cd0a;\n"
+        "uniform float webgl_73f8cb2ab947f459;\n"
+        "uniform float webgl_58eb32532f5ee481;\n"
+        "uniform float webgl_e05f6d076f9b3f32;\n"
+        "uniform float webgl_53848c7f1cf694ac;\n"
+        "uniform float webgl_b23f3037257feb4b;\n"
+        "uniform float webgl_da6e391939e86900;\n"
+        "uniform float webgl_982623471b1d160e;\n"
+        "uniform float webgl_c9cd165c64c355df;\n"
+        "uniform vec3 webgl_77bf37367d0ad46d;\n"
+        "uniform sampler2D webgl_3543ec1d70156653;\n"
+        "in vec2 webgl_51798873aa8ff428;\n"
+        "void main(){\n"
+        "vec4 webgl_19dff938713edbff, webgl_a8f0f678f99829f9, "
+        "webgl_86be61d1965e63b7, webgl_48c57da858454f79;\n"
+        "float webgl_55fdf3cc830b6c33, webgl_a34964a2a96b201f;\n"
+        "vec2 webgl_cb128f6fdfe0160b;\n"
+        "vec2 webgl_758cdc8b0df6c2a9 = webgl_51798873aa8ff428;\n"
+        "(webgl_48c57da858454f79 += (webgl_19dff938713edbff = "
+        "(webgl_a8f0f678f99829f9 = texture(webgl_3543ec1d70156653, "
+        "webgl_758cdc8b0df6c2a9))));\n"
+        "(webgl_cb128f6fdfe0160b = ((webgl_758cdc8b0df6c2a9 - vec2(0.5, 0.5)) "
+        "* vec2(1.55, 1.55)));\n"
+        "(webgl_55fdf3cc830b6c33 = (dot(webgl_cb128f6fdfe0160b, "
+        "webgl_cb128f6fdfe0160b) * 2.0));\n"
+        "(webgl_a34964a2a96b201f = ((((webgl_c9cd165c64c355df * 100.0) + "
+        "webgl_55fdf3cc830b6c33) * webgl_da6e391939e86900) * 4.0));\n"
+        "vec2 webgl_ebe00033516781cb = (vec2((1.0 / webgl_9f3d75d0c870cd0a), "
+        "(1.0 / webgl_73f8cb2ab947f459)) * vec2(webgl_a34964a2a96b201f));\n"
+        "(webgl_48c57da858454f79 += (webgl_86be61d1965e63b7 = "
+        "texture(webgl_3543ec1d70156653, (webgl_758cdc8b0df6c2a9 + "
+        "(vec2(0.111964, 0.99371201) * webgl_ebe00033516781cb)))));\n"
+        "if ((webgl_86be61d1965e63b7.y > webgl_19dff938713edbff.y))\n"
         "{\n"
-        "        color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+        "(webgl_19dff938713edbff = webgl_86be61d1965e63b7);\n"
+        "}\n"
+        "(webgl_48c57da858454f79 += (webgl_86be61d1965e63b7 = "
+        "texture(webgl_3543ec1d70156653, (webgl_758cdc8b0df6c2a9 + "
+        "(vec2(0.84672397, 0.53203201) * webgl_ebe00033516781cb)))));\n"
+        "if ((webgl_86be61d1965e63b7.y > webgl_19dff938713edbff.y))\n"
+        "{\n"
+        "(webgl_19dff938713edbff = webgl_86be61d1965e63b7);\n"
+        "}\n"
+        "(webgl_48c57da858454f79 += (webgl_86be61d1965e63b7 = "
+        "texture(webgl_3543ec1d70156653, (webgl_758cdc8b0df6c2a9 + "
+        "(vec2(0.943883, -0.33027899) * webgl_ebe00033516781cb)))));\n"
+        "if ((webgl_86be61d1965e63b7.y > webgl_19dff938713edbff.y))\n"
+        "{\n"
+        "(webgl_19dff938713edbff = webgl_86be61d1965e63b7);\n"
+        "}\n"
+        "(webgl_48c57da858454f79 += (webgl_86be61d1965e63b7 = "
+        "texture(webgl_3543ec1d70156653, (webgl_758cdc8b0df6c2a9 + "
+        "(vec2(0.33027899, -0.943883) * webgl_ebe00033516781cb)))));\n"
+        "if ((webgl_86be61d1965e63b7.y > webgl_19dff938713edbff.y))\n"
+        "{\n"
+        "(webgl_19dff938713edbff = webgl_86be61d1965e63b7);\n"
+        "}\n"
+        "(webgl_48c57da858454f79 += (webgl_86be61d1965e63b7 = "
+        "texture(webgl_3543ec1d70156653, (webgl_758cdc8b0df6c2a9 + "
+        "(vec2(-0.53203201, -0.84672397) * webgl_ebe00033516781cb)))));\n"
+        "if ((webgl_86be61d1965e63b7.y > webgl_19dff938713edbff.y))\n"
+        "{\n"
+        "(webgl_19dff938713edbff = webgl_86be61d1965e63b7);\n"
+        "}\n"
+        "(webgl_48c57da858454f79 += (webgl_86be61d1965e63b7 = "
+        "texture(webgl_3543ec1d70156653, (webgl_758cdc8b0df6c2a9 + "
+        "(vec2(-0.99371201, -0.111964) * webgl_ebe00033516781cb)))));\n"
+        "if ((webgl_86be61d1965e63b7.y > webgl_19dff938713edbff.y))\n"
+        "{\n"
+        "(webgl_19dff938713edbff = webgl_86be61d1965e63b7);\n"
+        "}\n"
+        "(webgl_48c57da858454f79 += (webgl_86be61d1965e63b7 = "
+        "texture(webgl_3543ec1d70156653, (webgl_758cdc8b0df6c2a9 + "
+        "(vec2(-0.70710701, 0.70710701) * webgl_ebe00033516781cb)))));\n"
+        "if ((webgl_86be61d1965e63b7.y > webgl_19dff938713edbff.y))\n"
+        "{\n"
+        "(webgl_19dff938713edbff = webgl_86be61d1965e63b7);\n"
+        "}\n"
+        "(webgl_758cdc8b0df6c2a9 = ((webgl_758cdc8b0df6c2a9 - "
+        "vec2(0.5, 0.5)) * vec2(webgl_58eb32532f5ee481)));\n"
+        "(webgl_19dff938713edbff = (webgl_19dff938713edbff + "
+        "(((webgl_48c57da858454f79 / vec4(8.0, 8.0, 8.0, 8.0)) - "
+        "webgl_19dff938713edbff) * (vec4(1.0, 1.0, 1.0, 1.0) - "
+        "vec4((webgl_55fdf3cc830b6c33 * 0.5))))));\n"
+        "(webgl_FragColor = vec4(mix((((webgl_19dff938713edbff.xyz * "
+        "webgl_19dff938713edbff.xyz) * vec3(webgl_53848c7f1cf694ac)) + "
+        "webgl_19dff938713edbff.xyz), ((webgl_19dff938713edbff.yyy * "
+        "webgl_b23f3037257feb4b) - vec3(webgl_e05f6d076f9b3f32)), "
+        "vec3(dot(webgl_758cdc8b0df6c2a9, webgl_758cdc8b0df6c2a9))), 1.0));\n"
+        "(webgl_FragColor = (vec4(1.0, 1.0, 1.0, 1.0) - "
+        "((vec4(1.0, 1.0, 1.0, 1.0) - webgl_FragColor) * "
+        "(vec4(1.0, 1.0, 1.0, 1.0) - webgl_FragColor))));\n"
         "}\n";
 
 struct data {
@@ -36,6 +127,7 @@ struct data {
 
         GLuint program;
         GLuint vao, vbo;
+        GLuint tex;
 
         bool quit;
         bool redraw_queued;
@@ -79,6 +171,11 @@ handle_event(struct data *data,
 static void
 handle_redraw(struct data *data)
 {
+        int w, h;
+
+        SDL_GetWindowSize(data->window, &w, &h);
+        glViewport(0, 0, w, h);
+
         glUseProgram(data->program);
         glBindVertexArray(data->vao);
         glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
@@ -262,6 +359,57 @@ deinit_vertices(struct data *data)
         glDeleteBuffers(1, &data->vbo);
 }
 
+static bool
+init_image(struct data *data)
+{
+        SDL_Surface *surface;
+        int alignment = 1;
+
+        surface = SDL_LoadBMP("image.bmp");
+        if (surface == NULL) {
+                fprintf(stderr, "image.bmp: %s\n", SDL_GetError());
+                return false;
+        }
+
+        if (surface->format->format != SDL_PIXELFORMAT_BGR24) {
+                fprintf(stderr, "Unknown pixel format in image: %s\n",
+                        SDL_GetPixelFormatName(surface->format->format));
+                goto error;
+        }
+
+        glGenTextures(1, &data->tex);
+        glBindTexture(GL_TEXTURE_2D, data->tex);
+        glPixelStorei(GL_UNPACK_ROW_LENGTH,
+                      surface->pitch / surface->format->BytesPerPixel);
+        while (alignment < surface->pitch % surface->format->BytesPerPixel)
+                alignment *= 2;
+        glPixelStorei(GL_UNPACK_ALIGNMENT, alignment);
+        glTexImage2D(GL_TEXTURE_2D,
+                     0, /* level */
+                     GL_RGB,
+                     surface->w,
+                     surface->h,
+                     0, /* border */
+                     GL_BGR,
+                     GL_UNSIGNED_BYTE,
+                     surface->pixels);
+        glGenerateMipmap(GL_TEXTURE_2D);
+
+        SDL_FreeSurface(surface);
+
+        return true;
+
+error:
+        SDL_FreeSurface(surface);
+        return false;
+}
+
+static void
+deinit_image(struct data *data)
+{
+        glDeleteTextures(1, &data->tex);
+}
+
 int
 main(int argc, char **argv)
 {
@@ -315,12 +463,20 @@ main(int argc, char **argv)
                 goto out_context;
         }
 
+        if (!init_image(&data)) {
+                ret = EXIT_FAILURE;
+                goto out_program;
+        }
+
         init_vertices(&data);
 
         run_main_loop(&data);
 
         deinit_vertices(&data);
 
+        deinit_image(&data);
+
+out_program:
         glUseProgram(0);
         glDeleteProgram(data.program);
 
